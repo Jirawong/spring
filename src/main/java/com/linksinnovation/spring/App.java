421beec20e911101cbdc82e878e5f0753b73aa96
Jirawong Wongdokpuang <Kong@Jirawongs-iMac.local>
2015-09-19 04:51:52 +0700

    Lab-SpringBeanAdditional

--- src/main/java/com/linksinnovation/spring/App.java
+++ src/main/java/com/linksinnovation/spring/App.java
@@ -5,10 +5,7 @@
  */
 package com.linksinnovation.spring;
 
-import com.linksinnovation.spring.service.BeanCaller1;
-import com.linksinnovation.spring.service.BeanCaller2;
-import com.linksinnovation.spring.service.ConstructorBean;
-import com.linksinnovation.spring.service.SetterBean;
+import com.linksinnovation.spring.service.DependOn1;
 import java.util.Arrays;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -23,10 +20,6 @@ public class App {
         
         System.out.println(Arrays.toString(context.getEnvironment().getActiveProfiles()));
         
-        ConstructorBean cb = context.getBean(ConstructorBean.class);
-        System.out.println(cb.getInjectBean().getMessage());
-        
-        SetterBean sb = context.getBean(SetterBean.class);
-        System.out.println(sb.getInjectBean().getMessage());
+        DependOn1 bean = context.getBean(DependOn1.class);
     }
 }
